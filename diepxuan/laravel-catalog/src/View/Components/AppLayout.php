09091e92997be8de821afce04b5bd3c090aa4a5c
Tran Ngoc Duc <caothu91@gmail.com>
2024-12-12 22:50:55 +0700

    create app layout

---
+++ diepxuan/laravel-catalog/src/View/Components/AppLayout.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * @copyright  © 2019 Dxvn, Inc.
+ *
+ * @author     Tran Ngoc Duc <ductn@diepxuan.com>
+ * @author     Tran Ngoc Duc <caothu91@gmail.com>
+ *
+ * @lastupdate 2024-12-12 22:11:30
+ */
+
+namespace Diepxuan\Catalog\View\Components;
+
+use Illuminate\View\Component;
+use Illuminate\View\View;
+
+class AppLayout extends Component
+{
+    /**
+     * Get the view / contents that represents the component.
+     */
+    public function render(): View
+    {
+        return view('catalog::layouts.app');
+    }
+}
