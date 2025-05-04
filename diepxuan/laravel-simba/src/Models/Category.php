3f7ebb4ed89ea854e17bf1dda42b0a4890d75d36
Tran Ngoc Duc <caothu91@gmail.com>
2025-05-04 18:51:48 +0700

    Update lastupdate timestamps and refactor Category and Product models

--- diepxuan/laravel-simba/src/Models/Category.php
+++ diepxuan/laravel-simba/src/Models/Category.php
@@ -8,16 +8,72 @@ declare(strict_types=1);
  * @author     Tran Ngoc Duc <ductn@diepxuan.com>
  * @author     Tran Ngoc Duc <caothu91@gmail.com>
  *
- * @lastupdate 2024-12-20 22:40:33
+ * @lastupdate 2025-05-04 18:42:34
  */
 
 namespace Diepxuan\Simba\Models;
 
 use Diepxuan\Simba\SModel\InDmNhvt;
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
 
 class Category extends InDmNhvt
 {
+    public const ROOT = 'PRODUCT';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    // protected $primaryKey = 'id';
+
+    /**
+     * Get the children Categories.
+     */
+    public function catChildrens(): HasMany
+    {
+        return $this->hasMany(self::class, 'nhom_me', 'ma_nhvt');
+    }
+
+    /**
+     * Get the parent Category.
+     */
+    public function catParent(): BelongsTo
+    {
+        return $this->belongsTo(self::class, 'nhom_me', 'ma_nhvt');
+    }
+
+    /**
+     * Parent scope.
+     *
+     * @param mixed $query
+     */
+    public function scopeIsRoot($query)
+    {
+        return $this->scopeHasParent($query, '');
+    }
+
+    /**
+     * Parent scope.
+     *
+     * @param mixed $query
+     * @param mixed $parent
+     */
+    public function scopeHasParent($query, $parent = '')
+    {
+        if ($parent instanceof self) {
+            return $query->where('nhom_me', $parent->sku);
+        }
+        if (\is_string($parent)) {
+            return $query->where('nhom_me', $parent);
+        }
+
+        return $query;
+    }
+
     /**
      * Get the Simba Category Id.
      */
@@ -39,6 +95,16 @@ class Category extends InDmNhvt
     }
 
     /**
+     * Get the Simba status.
+     */
+    protected function status(): Attribute
+    {
+        return Attribute::make(
+            get: static fn (mixed $value, array $attributes) => !$attributes['ksd'],
+        );
+    }
+
+    /**
      * Get the Simba Category parent.
      */
     protected function parent(): Attribute
@@ -55,6 +121,26 @@ class Category extends InDmNhvt
     {
         return Attribute::make(
             get: fn (mixed $value, array $attributes) => $this->ten_nhvt,
+        );
+    }
+
+    protected function Products(): HasMany
+    {
+        return $this->hasMany(Product::class, 'category', 'sku');
+    }
+
+    protected function urlPath(): Attribute
+    {
+        return Attribute::make(
+            get: fn (mixed $value, array $attributes) => $this->catParent ? ($this->catParent->isRoot ? "{$this->urlKey}" : "{$this->catParent->urlPath}/{$this->urlKey}") : '',
+        );
+    }
+
+    protected function urlKey(): Attribute
+    {
+        return Attribute::make(
+            get: fn (mixed $value, array $attributes) => $value ?: ($this->isRoot ? '' : Str::of(vn_convert_encoding($this->name))->slug('-')),
+            set: static fn (string $value, array $attributes) => strtolower($value),
         );
     }
 }
--- diepxuan/laravel-simba/src/Models/Product.php
+++ diepxuan/laravel-simba/src/Models/Product.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
  * @author     Tran Ngoc Duc <ductn@diepxuan.com>
  * @author     Tran Ngoc Duc <caothu91@gmail.com>
  *
- * @lastupdate 2025-03-30 08:13:11
+ * @lastupdate 2025-05-04 17:58:29
  */
 
 namespace Diepxuan\Simba\Models;
@@ -27,6 +27,8 @@ class Product extends InDmVt
      */
     protected $casts = [
         'quantity' => 'decimal:1',
+        'gia_nt2'  => 'float',
+        'price'    => 'float',
     ];
 
     public function scopeWithQuantityOld($query, $flag = true)
@@ -153,6 +155,21 @@ class Product extends InDmVt
         return Attribute::make(
             get: static fn (mixed $value, array $attributes) => !$attributes['ksd'],
         );
+    }
+
+    protected function urlKey(): Attribute
+    {
+        return Attribute::make(
+            get: static fn (mixed $value, array $attributes) => Str::of(vn_convert_encoding($this->name))->slug('-'),
+        );
+    }
+
+    /**
+     * Get the Category.
+     */
+    protected function cat(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'ma_nhvt', 'ma_nhvt');
     }
 
     /**
