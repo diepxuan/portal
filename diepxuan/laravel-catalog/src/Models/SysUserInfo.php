24da86420ddfcb9099c91f480c2fe0a5216a799b
Tran Ngoc Duc <caothu91@gmail.com>
2025-05-21 10:44:18 +0700

    create base model

---
+++ diepxuan/laravel-catalog/src/Models/SysUserInfo.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * @copyright  © 2019 Dxvn, Inc.
+ *
+ * @author     Tran Ngoc Duc <ductn@diepxuan.com>
+ * @author     Tran Ngoc Duc <caothu91@gmail.com>
+ *
+ * @lastupdate 2025-05-21 07:40:59
+ */
+
+namespace Diepxuan\Catalog\Models;
+
+use Diepxuan\Simba\Models\SysUserInfo as Model;
+
+class SysUserInfo extends Model {}
