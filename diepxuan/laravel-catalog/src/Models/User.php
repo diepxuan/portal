768c063168d97fffd085088694b31b346a8e4bff
Tran Ngoc Duc <caothu91@gmail.com>
2025-05-21 18:24:55 +0700

    Add user links migration, update User model relationships, and configure auth provider

--- diepxuan/laravel-catalog/src/Models/User.php
+++ diepxuan/laravel-catalog/src/Models/User.php
@@ -8,11 +8,31 @@ declare(strict_types=1);
  * @author     Tran Ngoc Duc <ductn@diepxuan.com>
  * @author     Tran Ngoc Duc <caothu91@gmail.com>
  *
- * @lastupdate 2025-05-20 11:44:35
+ * @lastupdate 2025-05-21 17:09:58
  */
 
 namespace Diepxuan\Catalog\Models;
 
-use Diepxuan\Simba\Models\SysLanguage as SsysLanguage;
+use App\Models\User as Model;
+use Diepxuan\Simba\Models\SysUserInfo;
+use Diepxuan\Simba\Models\UserLink;
 
-class User extends SsysLanguage {}
+class User extends Model
+{
+    public function simbaLink()
+    {
+        return $this->hasOne(UserLink::class, 'laravel_user_id');
+    }
+
+    public function simbaUser()
+    {
+        return $this->hasOneThrough(
+            SysUserInfo::class,
+            UserLink::class,
+            'laravel_user_id',   // Foreign key on UserLink
+            'username',            // Foreign key on SysUserInfo
+            'id',                // Local key on User
+            'simba_user_id'      // Local key on UserLink
+        );
+    }
+}
