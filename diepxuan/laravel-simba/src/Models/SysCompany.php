22ee05d901489ae496cf452ee8f060b35da354d1
Tran Ngoc Duc <caothu91@gmail.com>
2025-05-21 10:44:18 +0700

    create base model

---
+++ diepxuan/laravel-simba/src/Models/SysCompany.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * @copyright  © 2019 Dxvn, Inc.
+ *
+ * @author     Tran Ngoc Duc <ductn@diepxuan.com>
+ * @author     Tran Ngoc Duc <caothu91@gmail.com>
+ *
+ * @lastupdate 2025-05-21 07:38:47
+ */
+
+namespace Diepxuan\Simba\Models;
+
+use Diepxuan\Simba\SModel\SysCompany as Model;
+
+class SysCompany extends Model {}
---
+++ diepxuan/laravel-simba/src/SModel/SysUserInfo.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * @copyright  © 2019 Dxvn, Inc.
+ *
+ * @author     Tran Ngoc Duc <ductn@diepxuan.com>
+ * @author     Tran Ngoc Duc <caothu91@gmail.com>
+ *
+ * @lastupdate 2025-05-20 12:21:33
+ */
+
+namespace Diepxuan\Simba\SModel;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SysUserInfo extends SModel
+{
+    public const CREATED_AT = 'cDate';
+    public const UPDATED_AT = 'lDate';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'sysUserInfo';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'username';
+
+    /**
+     * The "type" of the primary key ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+}
