55956aa881f8e40b79e3b8fef9456863ad4ec6c4
Tran Ngoc Duc <caothu91@gmail.com>
2025-05-24 16:55:38 +0700

    Refactor user retrieval logic in SysUserInfo component

--- diepxuan/laravel-catalog/src/View/Components/SysUserInfo.php
+++ diepxuan/laravel-catalog/src/View/Components/SysUserInfo.php
@@ -8,13 +8,12 @@ declare(strict_types=1);
  * @author     Tran Ngoc Duc <ductn@diepxuan.com>
  * @author     Tran Ngoc Duc <caothu91@gmail.com>
  *
- * @lastupdate 2025-05-24 07:59:39
+ * @lastupdate 2025-05-24 16:53:36
  */
 
 namespace Diepxuan\Catalog\View\Components;
 
 use Diepxuan\Catalog\Models\User;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\View\Component;
 use Illuminate\View\View;
 
@@ -26,7 +25,8 @@ class SysUserInfo extends Component
      */
     public function render(): string|View
     {
-        $simbaUser = optional(Auth::user()->getSimbaUser())->username;
+        // $simbaUser = optional(catalog()->user()->getSimbaUser())->username;
+        $simbaUser = optional(\CatalogService::simbaUser())->username;
 
         return <<<HTML
                 <span>{$simbaUser}</span>
