3468dd0ea7ee6ee3d260e16ccffb0d474a894311
Tran Ngoc Duc <caothu91@gmail.com>
2025-05-27 19:44:13 +0700

    Add GlDmTk model definition

---
+++ diepxuan/laravel-catalog/src/Models/GlDmTk.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * @copyright  © 2019 Dxvn, Inc.
+ *
+ * @author     Tran Ngoc Duc <ductn@diepxuan.com>
+ * @author     Tran Ngoc Duc <caothu91@gmail.com>
+ *
+ * @lastupdate 2025-05-27 18:08:14
+ */
+
+namespace Diepxuan\Catalog\Models;
+
+use Diepxuan\Simba\Models\GlDmTk as Model;
+
+class GlDmTk extends Model {}
