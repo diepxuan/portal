8291bc651c6621163dff88732b48e5fdbf2ec7f9
Tran Ngoc Duc <caothu91@gmail.com>
2025-05-27 19:53:52 +0700

    Add Taikhoan Livewire component and update navigation menu

---
+++ diepxuan/laravel-catalog/resources/views/gl/taikhoan.blade.php
@@ -0,0 +1,11 @@
+<div>
+    <x-head-title>{{ __('Tổng hợp') }}</x-head-title>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Tổng hợp') }}
+        </h2>
+        <p>{{ __('Tài khoản kế toán') }}</p>
+    </x-slot>
+
+    {{-- @livewire('gl.taikhoan.index') --}}
+</div>
--- diepxuan/laravel-catalog/routes/web.php
+++ diepxuan/laravel-catalog/routes/web.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
  * @author     Tran Ngoc Duc <ductn@diepxuan.com>
  * @author     Tran Ngoc Duc <caothu91@gmail.com>
  *
- * @lastupdate 2025-05-27 11:24:41
+ * @lastupdate 2025-05-27 19:24:59
  */
 
 use Diepxuan\Catalog\Http\Controllers\CatalogController;
@@ -18,6 +18,7 @@ use Diepxuan\Catalog\Http\Controllers\SellController;
 use Diepxuan\Catalog\Http\Controllers\SystemController;
 use Diepxuan\Catalog\Http\Controllers\SystemUserController;
 use Diepxuan\Catalog\Http\Controllers\SystemWebsiteController;
+use Diepxuan\Catalog\Http\Livewire\Gl\Taikhoan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -31,6 +32,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::domain('portal.diepxuan.io.vn')->middleware(['clearcache', 'auth'])->group(static function (): void {
+    Route::get('/gl/taikhoan', Taikhoan::class)->name('gl.taikhoan');
+
     Route::get('/cash/tienmat/thu', static fn () => view('catalog::cash.tienmat.phieuthu'))->name('ca.tienmat.thu');
     Route::get('/cash/tienmat/chi', static fn () => view('catalog::cash.tienmat.phieuchi'))->name('ca.tienmat.chi');
     Route::get('/cash/tienmat/quy', static fn () => view('catalog::dashboard'))->name('ca.tienmat.quy');
