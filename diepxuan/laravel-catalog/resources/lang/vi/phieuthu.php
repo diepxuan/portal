8f1bbd2c2cd3edfd8b883470e0a1aca604c7f309
Tran Ngoc Duc <caothu91@gmail.com>
2025-05-31 15:07:49 +0700

    Add Vietnamese language translations for 'phieuthu' and update translation loading logic in TranslationServiceProvider

---
+++ diepxuan/laravel-catalog/resources/lang/vi/phieuthu.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * @copyright  © 2019 Dxvn, Inc.
+ *
+ * @author     Tran Ngoc Duc <ductn@diepxuan.com>
+ * @author     Tran Ngoc Duc <caothu91@gmail.com>
+ *
+ * @lastupdate 2025-05-31 13:49:46
+ */
+
+return [
+    'change' => 'Tiền tệ',
+    'cash'   => 'Tiền mặt',
+    'bank'   => 'Ngân hàng',
+    'create' => 'Tạo phiếu thu',
+    'edit'   => 'Chỉnh sửa phiếu thu',
+];
