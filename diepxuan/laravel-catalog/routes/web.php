048f588a4681631d69e6f30a445085715da6bed2
Tran Ngoc Duc <caothu91@gmail.com>
2025-06-01 17:42:53 +0000

    Add Baono component and view for cash management report

--- diepxuan/laravel-catalog/routes/web.php
+++ diepxuan/laravel-catalog/routes/web.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
  * @author     Tran Ngoc Duc <ductn@diepxuan.com>
  * @author     Tran Ngoc Duc <caothu91@gmail.com>
  *
- * @lastupdate 2025-06-01 17:07:31
+ * @lastupdate 2025-06-02 00:42:11
  */
 
 use Diepxuan\Catalog\Http\Controllers\CatalogController;
@@ -19,6 +19,8 @@ use Diepxuan\Catalog\Http\Controllers\SystemController;
 use Diepxuan\Catalog\Http\Controllers\SystemUserController;
 use Diepxuan\Catalog\Http\Controllers\SystemWebsiteController;
 use Diepxuan\Catalog\Http\Livewire\Banhang\Khachhang;
+use Diepxuan\Catalog\Http\Livewire\Cash\Nganhang\Baoco;
+use Diepxuan\Catalog\Http\Livewire\Cash\Nganhang\Baono;
 use Diepxuan\Catalog\Http\Livewire\Cash\Tienmat\Phieuchi;
 use Diepxuan\Catalog\Http\Livewire\Cash\Tienmat\Phieuthu;
 use Diepxuan\Catalog\Http\Livewire\Gl\Taikhoan;
@@ -40,8 +42,8 @@ Route::domain('portal.diepxuan.io.vn')->middleware(['clearcache', 'auth'])->grou
     Route::get('/cash/tienmat/thu', Phieuthu::class)->name('ca.tienmat.thu');
     Route::get('/cash/tienmat/chi', Phieuchi::class)->name('ca.tienmat.chi');
     Route::get('/cash/tienmat/quy', static fn () => view('catalog::dashboard'))->name('ca.tienmat.quy');
-    Route::get('/cash/nganhang/baoco', static fn () => view('catalog::dashboard'))->name('ca.nganhang.baoco');
-    Route::get('/cash/nganhang/baono', static fn () => view('catalog::dashboard'))->name('ca.nganhang.baono');
+    Route::get('/cash/nganhang/baoco', Baoco::class)->name('ca.nganhang.baoco');
+    Route::get('/cash/nganhang/baono', Baono::class)->name('ca.nganhang.baono');
     Route::get('/cash/nganhang/quy', static fn () => view('catalog::dashboard'))->name('ca.nganhang.quy');
     Route::get('/cash/thu', static fn () => view('catalog::dashboard'))->name('ca.thu');
     Route::get('/cash/chi', static fn () => view('catalog::dashboard'))->name('ca.chi');
