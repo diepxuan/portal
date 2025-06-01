001893c51e68d80c169791a4be5b48a54abab8a1
Tran Ngoc Duc <caothu91@gmail.com>
2025-06-01 13:05:11 +0000

    Update lastupdate timestamps and implement relationship methods in ArDmKh and GlCt models

--- diepxuan/laravel-simba/src/Models/ArDmKh.php
+++ diepxuan/laravel-simba/src/Models/ArDmKh.php
@@ -8,11 +8,18 @@ declare(strict_types=1);
  * @author     Tran Ngoc Duc <ductn@diepxuan.com>
  * @author     Tran Ngoc Duc <caothu91@gmail.com>
  *
- * @lastupdate 2025-06-01 17:02:20
+ * @lastupdate 2025-06-01 19:56:38
  */
 
 namespace Diepxuan\Simba\Models;
 
 use Diepxuan\Simba\SModel\ArDmKh as Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class ArDmKh extends Model {}
+class ArDmKh extends Model
+{
+    public function glCts(): HasMany
+    {
+        return $this->hasMany(GlCt::class, 'ma_kh', 'ma_kh');
+    }
+}
