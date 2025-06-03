32e79782fdb25926594f911cadf195f08158b0c2
Tran Ngoc Duc <caothu91@gmail.com>
2025-06-03 09:20:27 +0000

    Add Tienmat component and view for cash report functionality

---
+++ diepxuan/laravel-catalog/src/Http/Livewire/Cash/Baocao/Tienmat.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * @copyright  © 2019 Dxvn, Inc.
+ *
+ * @author     Tran Ngoc Duc <ductn@diepxuan.com>
+ * @author     Tran Ngoc Duc <caothu91@gmail.com>
+ *
+ * @lastupdate 2025-06-03 16:13:35
+ */
+
+namespace Diepxuan\Catalog\Http\Livewire\Cash\Baocao;
+
+use Diepxuan\Catalog\Models\GlCt;
+use Illuminate\View\View;
+use Livewire\Component;
+
+class Tienmat extends Component
+{
+    public $pTk    = '111';
+    public $pMa_Nt = 'VND';
+    protected $glCts;
+
+    public function mount(): void
+    {
+        \Debugbar::info(\CatalogService::timerFrom());
+        \Debugbar::info(\CatalogService::timerTo());
+    }
+
+    public function updated($property): void
+    {
+        // \Debugbar::info($property);
+    }
+
+    public function submit(): void
+    {
+        $this->glCts = GlCt::getCARptTMNH01([
+            'ma_Cty'   => \CatalogService::company()->id,
+            'ngay_Ct1' => \CatalogService::timerFrom(),
+            'ngay_Ct2' => \CatalogService::timerTo(),
+            'tk'       => $this->pTk,
+            'ma_Nt'    => 'VND',
+        ])->get();
+    }
+
+    /**
+     * Render the component.
+     *
+     * @return View
+     */
+    public function render()
+    {
+        // diepxuan/laravel-catalog/resources/views/cash/baocao/tienmat.blade.php
+        return view('catalog::cash.baocao.tienmat', [
+            'glCts' => $this->glCts,
+        ])->layout('catalog::layouts.app');
+    }
+}
