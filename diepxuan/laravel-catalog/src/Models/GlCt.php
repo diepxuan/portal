40fe95cf408633b26f4b2fbe4ad0eb435fff11e8
Tran Ngoc Duc <caothu91@gmail.com>
2025-06-03 13:39:44 +0000

    Refactor Tienmat report view and update related model methods for cash report functionality

--- diepxuan/laravel-catalog/src/Models/GlCt.php
+++ diepxuan/laravel-catalog/src/Models/GlCt.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
  * @author     Tran Ngoc Duc <ductn@diepxuan.com>
  * @author     Tran Ngoc Duc <caothu91@gmail.com>
  *
- * @lastupdate 2025-06-01 20:09:35
+ * @lastupdate 2025-06-03 17:27:55
  */
 
 namespace Diepxuan\Catalog\Models;
@@ -21,6 +21,33 @@ use Illuminate\Support\Collection;
 
 class GlCt extends Model
 {
+    /**
+     * Gọi stored procedure asCARptTMNH01 để lấy dữ liệu báo cáo tiền mặt ngân hàng.
+     *
+     * @return array
+     */
+    public function scopeGetCARptTMNH(Builder $query, array $params): Builder
+    {
+        $params = [
+            'pMa_Cty'   => $params['ma_Cty'] ?? '111',
+            'pTk'       => $params['tk'] ?? '111',
+            'pNgay_Ct1' => $params['ngay_Ct1'],
+            'pNgay_Ct2' => $params['ngay_Ct2'],
+            'pMa_Nt'    => $params['ma_Nt'] ?? 'VND',
+        ];
+
+        return $query
+            ->filterByMaCty($params['pMa_Cty'])
+            ->filterByNgayCt($params['pNgay_Ct1'], $params['pNgay_Ct2'])
+            ->filterByTkList($params['pTk'] ?? '')
+            ->filterByMaNt($params['pMa_Nt'] ?? '')
+            ->with('arDmKh')
+            ->orderBy('ngay_ct')
+            ->orderBy('so_ct')
+            ->orderBy('stt_rec')
+        ;
+    }
+
     /**
      * Gọi stored procedure asGLRptNKC03 để lấy dữ liệu sổ nhật ký thu/chi.
      *
