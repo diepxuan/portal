97965ced20f5713d41fd0f94645ed397bf7b8647
Tran Ngoc Duc <caothu91@gmail.com>
2025-06-05 00:41:17 +0000

    Add GlCdTk model and update SModel for general ledger transactions

---
+++ diepxuan/laravel-catalog/src/Models/GlCdTk.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * @copyright  © 2019 Dxvn, Inc.
+ *
+ * @author     Tran Ngoc Duc <ductn@diepxuan.com>
+ * @author     Tran Ngoc Duc <caothu91@gmail.com>
+ *
+ * @lastupdate 2025-06-04 22:12:58
+ */
+
+namespace Diepxuan\Catalog\Models;
+
+use Diepxuan\Simba\Models\GlCdTk as Model;
+
+class GlCdTk extends Model {}
