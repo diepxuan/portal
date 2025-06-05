2b87001bb4a725778145517fce9ca7da647e87c4
Tran Ngoc Duc <caothu91@gmail.com>
2025-06-05 00:41:17 +0000

    Add GlCdTk model and update SModel for general ledger transactions

---
+++ diepxuan/laravel-simba/src/Models/GlCdTk.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * @copyright  © 2019 Dxvn, Inc.
+ *
+ * @author     Tran Ngoc Duc <ductn@diepxuan.com>
+ * @author     Tran Ngoc Duc <caothu91@gmail.com>
+ *
+ * @lastupdate 2025-06-04 22:10:48
+ */
+
+namespace Diepxuan\Simba\Models;
+
+use Diepxuan\Simba\SModel\GlCdTk as Model;
+
+/**
+ * Class GlCdTk.
+ *
+ * This class represents the model for the general ledger transactions.
+ * It provides methods to interact with the stored procedure for generating reports.
+ */
+class GlCdTk extends Model {}
