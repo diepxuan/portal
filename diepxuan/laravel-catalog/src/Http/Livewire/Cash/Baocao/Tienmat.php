e264274224f8641af88267ef124ee233c0d7ac25
Tran Ngoc Duc <caothu91@gmail.com>
2025-06-09 09:46:16 +0000

    Update Tienmat view and related models: add beginning and ending balance calculations, enhance data retrieval methods, and update lastupdate timestamps

--- diepxuan/laravel-catalog/src/Http/Livewire/Cash/Baocao/Tienmat.php
+++ diepxuan/laravel-catalog/src/Http/Livewire/Cash/Baocao/Tienmat.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
  * @author     Tran Ngoc Duc <ductn@diepxuan.com>
  * @author     Tran Ngoc Duc <caothu91@gmail.com>
  *
- * @lastupdate 2025-06-05 14:50:16
+ * @lastupdate 2025-06-09 16:41:24
  */
 
 namespace Diepxuan\Catalog\Http\Livewire\Cash\Baocao;
@@ -21,6 +21,8 @@ class Tienmat extends Component
 {
     public $pTk    = '111';
     public $pMa_Nt = 'VND';
+    public $duCk;
+    public $glCtDk;
     protected $glCts;
 
     public function mount(): void
@@ -28,14 +30,23 @@ class Tienmat extends Component
         // \Debugbar::info(\CatalogService::timerFrom());
         // \Debugbar::info(\CatalogService::timerTo());
         // \Debugbar::info(\CatalogService::company()->siSetup->ngay_dntc);
-        \Debugbar::info(\CatalogFunctions::afNamTC([
-            'ma_cty' => \CatalogService::company()->ma_cty,
-            'ngay'   => \CatalogService::timerFrom(),
-        ]));
-        \Debugbar::info(\CatalogFunctions::afDuDauTk([
-            'pMaCty' => \CatalogService::company()->ma_cty,
-            'pNgay'  => \CatalogService::timerFrom(),
-        ]));
+        // \Debugbar::info(\CatalogFunctions::afNamTC([
+        //     'ma_cty' => \CatalogService::company()->ma_cty,
+        //     'ngay'   => \CatalogService::timerFrom(),
+        // ]));
+        // $glCtDk = \CatalogFunctions::afDuDauTk([
+        //     'pMaCty' => \CatalogService::company()->ma_cty,
+        //     'pNgay'  => \CatalogService::timerFrom(),
+        // ]);
+        // $this->glCtDk = [
+        //     'du_no'    => $glCtDk->sum(static fn ($i) => (float) $i->du_no - (float) $i->du_co),
+        //     'du_co'    => $glCtDk->sum(static fn ($i) => 0),
+        //     'du_no_nt' => $glCtDk->sum(static fn ($i) => (float) $i->du_no_nt - (float) $i->du_co_nt),
+        //     'du_co_nt' => $glCtDk->sum(static fn ($i) => 0),
+        // ];
+        // \Debugbar::info(
+        //     $glCtDk
+        // );
     }
 
     public function updated($property): void
@@ -45,13 +56,27 @@ class Tienmat extends Component
 
     public function submit(): void
     {
+        $this->glCtDk = \CatalogFunctions::afDuDauTk([
+            'pMaCty' => \CatalogService::company()->ma_cty,
+            'pNgay'  => \CatalogService::timerFrom(),
+        ]);
+        $this->duCk  = $this->glCtDk->du_no - $this->glCtDk->du_co;
         $this->glCts = GlCt::getCARptTMNH([
             'ma_Cty'   => \CatalogService::company()->id,
             'ngay_Ct1' => \CatalogService::timerFrom(),
             'ngay_Ct2' => \CatalogService::timerTo(),
             'tk'       => $this->pTk,
             'ma_Nt'    => \CatalogService::ma_Nt(),
-        ])->get();
+        ])
+            ->get()
+            ->map(function ($glCt) {
+                $this->duCk += (float) $glCt->ps_no - (float) $glCt->ps_co;
+                // \Debugbar::info($this->duCk);
+                $glCt->ton = (float) $this->duCk;
+
+                return $glCt;
+            })
+        ;
     }
 
     /**
