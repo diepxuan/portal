b498f5454d2cdfb4b42a470d10c231e5415349a6
caothu91 <caothu91@gmail.com>
2025-06-11 04:51:17 +0000

    idx config

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -4,26 +4,50 @@
     pkgs.nodejs_20
     pkgs.php
     pkgs.php82Packages.composer
+    pkgs.openssl
+    pkgs.libjpeg
+    pkgs.libpng
+    pkgs.vips              # xử lý ảnh
+    pkgs.git
+    pkgs.cloudflared
   ];
   idx.extensions = [
     "svelte.svelte-vscode"
     "vue.volar"
+    "ms-vscode.sublime-keybindings"
   ];
   idx.previews = {
+    enable=true;
     previews = {
       web = {
         command = [
-          "npm"
-          "run"
-          "dev"
-          "--"
+          "php"
+          "artisan"
+          "serve"
           "--port"
           "$PORT"
           "--host"
           "0.0.0.0"
         ];
         manager = "web";
+        # Preview cổng 8000
+        # port = 8000;
       };
+    };
+  };
+
+  idx.workspace = {
+    onCreate = {
+      npm-install = "npm install";
+      composer-install = "composer install";
+    };
+    onStart = {
+      # npm-watch-fe = "npm run watch:frontend";
+      npm-dev = "npm run dev";
+      mysql-demon = "${pkgs.cloudflared}/bin/cloudflared access tcp --hostname mysql.diepxuan.io.vn --url 127.0.0.1:3306";
+      sql-demon = "${pkgs.cloudflared}/bin/cloudflared access tcp --hostname sql.diepxuan.io.vn --url 127.0.0.1:1433";
+      # php-server = "php artisan serve";
+      log-laravel = "tail -f storage/logs/laravel.log";
     };
   };
 }
---
+++ Procfile
@@ -0,0 +1,3 @@
+# Procfile
+web: php artisan serve --host=0.0.0.0 --port=8000
+vite: npm run dev -- --host=0.0.0.0 --port=5173
