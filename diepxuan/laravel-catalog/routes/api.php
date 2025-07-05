3b4396b0a82aa068d1492a6c7d910ff55ea0ba66
caothu91 <caothu91@gmail.com>
2025-07-05 13:33:17 +0000

    apt de lay danh sach hoa don

--- diepxuan/laravel-catalog/routes/api.php
+++ diepxuan/laravel-catalog/routes/api.php
@@ -8,9 +8,10 @@ declare(strict_types=1);
  * @author     Tran Ngoc Duc <ductn@diepxuan.com>
  * @author     Tran Ngoc Duc <caothu91@gmail.com>
  *
- * @lastupdate 2025-04-01 17:27:14
+ * @lastupdate 2025-07-03 19:22:04
  */
 
+use Diepxuan\Catalog\Http\Api\SoPh3Controller;
 use Diepxuan\Catalog\Http\Controllers\CatalogController;
 use Diepxuan\Catalog\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
@@ -30,4 +31,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->prefix('v1')->group(static function (): void {
     Route::apiResource('products', CatalogController::class)->names('api.product');
     Route::apiResource('categories', CategoryController::class)->names('api.category');
+
+    Route::apiResource('SoPh3', SoPh3Controller::class);
 });
