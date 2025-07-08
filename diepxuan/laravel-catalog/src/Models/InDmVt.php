508d02414b44cbcb14cd92ca3a6a34d0b411c2f7
caothu91 <caothu91@gmail.com>
2025-07-08 07:16:19 +0000

    hien thi danh sach vat tu

---
+++ diepxuan/laravel-catalog/src/Models/InDmVt.php
@@ -0,0 +1,97 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * @copyright  © 2019 Dxvn, Inc.
+ *
+ * @author     Tran Ngoc Duc <ductn@diepxuan.com>
+ * @author     Tran Ngoc Duc <caothu91@gmail.com>
+ *
+ * @lastupdate 2025-07-07 13:22:42
+ */
+
+namespace Diepxuan\Catalog\Models;
+
+use Diepxuan\Catalog\Observers\InDmVtObserver;
+use Diepxuan\Simba\Models\InDmVt as Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+#[ObservedBy([InDmVtObserver::class])]
+class InDmVt extends Model
+{
+    /**
+     * Scope a query to include the quantity of products.
+     *
+     * @var array
+     *
+     * @param mixed $query
+     * @param mixed $flag
+     */
+    public function scopeWithQuantity($query, $flag = true)
+    {
+        if (!$flag) {
+            return $query;
+        }
+        // Lấy danh sách cột từ bảng InDmVt, tránh cột tính toán và kiểu dữ liệu không hỗ trợ GROUP BY
+        // $connect        = DB::connection((new static())->getConnectionName());
+        $columns        = Schema::connection($this->connection)->getColumnListing($this->table);
+        $groupByColumns = array_diff($columns, ['quantity', 'Anh']); // Bỏ cột TEXT, NTEXT, IMAGE
+        $groupByColumns = array_map(fn ($col) => "{$this->table}.{$col}", $groupByColumns);
+
+        // Danh sách cột SELECT
+        $selectColumns = array_merge(
+            ["{$this->table}.ma_vt", "{$this->table}.ten_vt"],
+            $groupByColumns,
+            [
+                DB::raw('COALESCE(SUM(incdvt.so_luong), 0) + COALESCE(SUM(inct.sl_nhap_qd - inct.sl_xuat_qd), 0) AS quantity'),
+            ]
+        );
+
+        return $query->leftJoin('incdvt', function ($join): void {
+            $join->on("{$this->table}.ma_vt", '=', 'incdvt.ma_vt')
+                ->where('incdvt.nam', '=', DB::raw('YEAR(GETDATE())'))
+            ;
+        })
+            ->leftJoin('inct', function ($join): void {
+                $join->on("{$this->table}.ma_vt", '=', 'inct.ma_vt')
+                    ->where('inct.ngay_ct', '>=', DB::raw('DATEADD(YEAR, DATEDIFF(YEAR, 0, GETDATE()), 0)'))
+                ;
+            })
+            ->where("{$this->table}.ma_cty", '001') // Chỉ lấy dữ liệu của công ty 001
+            ->select($selectColumns)
+            ->groupBy($groupByColumns)
+        ;
+    }
+
+    protected function cdate(): Attribute
+    {
+        return Attribute::get(
+            static fn ($value, array $attributes) => optional(Carbon::parse($value ?? $attributes['cdate'] ?? $attributes['cDate'] ?? null))->format('d/m/y')
+        );
+    }
+
+    protected function ldate(): Attribute
+    {
+        return Attribute::get(
+            static fn ($value, array $attributes) => optional(Carbon::parse($value ?? $attributes['ldate'] ?? $attributes['lDate'] ?? null))->format('d/m/y')
+        );
+    }
+
+    protected function cuser(): Attribute
+    {
+        return Attribute::get(
+            static fn ($value, array $attributes) => $value ?? $attributes['cuser'] ?? $attributes['cUser']
+        );
+    }
+
+    protected function luser(): Attribute
+    {
+        return Attribute::get(
+            static fn ($value, array $attributes) => $value ?? $attributes['luser'] ?? $attributes['lUser']
+        );
+    }
+}
