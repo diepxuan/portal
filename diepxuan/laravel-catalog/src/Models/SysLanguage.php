86663bb1519f176935b273a31b4292b491c0db74
caothu91 <caothu91@gmail.com>
2025-07-12 16:05:00 +0000

    use stored procedure

--- diepxuan/laravel-catalog/src/Models/SysLanguage.php
+++ diepxuan/laravel-catalog/src/Models/SysLanguage.php
@@ -8,11 +8,20 @@ declare(strict_types=1);
  * @author     Tran Ngoc Duc <ductn@diepxuan.com>
  * @author     Tran Ngoc Duc <caothu91@gmail.com>
  *
- * @lastupdate 2025-05-20 11:43:34
+ * @lastupdate 2025-07-12 15:33:30
  */
 
 namespace Diepxuan\Catalog\Models;
 
 use Diepxuan\Simba\Models\SysLanguage as Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
-class SysLanguage extends Model {}
+class SysLanguage extends Model
+{
+    protected function name(): Attribute
+    {
+        return Attribute::get(
+            static fn ($value, array $attributes) => $value ?? $attributes['name'] ?? $attributes['Name'] ?? null
+        );
+    }
+}
