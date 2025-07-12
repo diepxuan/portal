65c8b838001c67bfed452a9567a917e6e0200448
caothu91 <caothu91@gmail.com>
2025-07-12 16:05:00 +0000

    use stored procedure

--- diepxuan/laravel-support/src/Collection.php
+++ diepxuan/laravel-support/src/Collection.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
  * @author     Tran Ngoc Duc <ductn@diepxuan.com>
  * @author     Tran Ngoc Duc <caothu91@gmail.com>
  *
- * @lastupdate 2025-07-12 15:13:44
+ * @lastupdate 2025-07-12 23:00:45
  */
 
 namespace Diepxuan\Support;
@@ -18,10 +18,10 @@ use Illuminate\Support\Str;
 
 class Collection extends BaseCollection
 {
-    public function toMarkdownTable(?array $headers = null): string
+    public function toMarkdownTable(?array $headers = null, $iteration = false): string
     {
         $collection     = $this;
-        $displayHeaders = $headers ?? [];
+        $displayHeaders = ($iteration ? ['iteration' => ''] : []) + $headers ?? [];
         $columnWidths   = [];
         $markdown       = '';
 
@@ -29,58 +29,71 @@ class Collection extends BaseCollection
             return '';
         }
 
-        foreach ($collection as $item) {
-            $row = match (\gettype($item)) {
-                'array' => (object) $item,
-                default => $item,
-            };
-            foreach ($displayHeaders as $key => $headerText) {
-                $rawValue    = $row->{$key} ?? null;
-                $stringValue = match (\gettype($rawValue)) {
-                    'boolean' => $rawValue ? '1' : '0',
-                    'NULL'    => '',
-                    'string'  => (string) str_replace('|', '\|', $rawValue),
-                    default   => (string) $rawValue,
+        $columnWidths = collect($displayHeaders)
+            ->map(static function ($headerText, $key) use ($collection) {
+                if ('iteration' === $key) {
+                    $rawValue       = $collection->count();
+                    $rawValueLength = Str::of("{$rawValue}")->length();
+                } else {
+                    $rawValueLength = $collection
+                        ->max(static function ($item) use ($key) {
+                            $item = match (\gettype($item)) {
+                                'array' => (object) $item,
+                                default => $item,
+                            };
+
+                            return Str::of($item->{$key} ?? null)->length();
+                        })
+                    ;
+                }
+
+                return max(Str::of($headerText)->length(), $rawValueLength);
+            })
+        ;
+
+        $markdownHeader = collect($displayHeaders)
+            ->map(static fn ($headerText, $key) => Str::of($headerText)->padRight($columnWidths[$key]))
+            ->implode(' | ')
+        ;
+        $markdown .= "| {$markdownHeader} |\n";
+
+        $markdownDelimiter = collect($displayHeaders)
+            ->map(static fn ($headerText, $key) => Str::of('')->padRight($columnWidths[$key], '-'))
+            ->implode(' | ')
+        ;
+        $markdown .= "| {$markdownDelimiter} |\n";
+
+        $markdownContent = $collection->values()
+            ->map(static function ($item, $iteration) use ($displayHeaders, $columnWidths) {
+                $row = match (\gettype($item)) {
+                    'array' => (object) $item,
+                    default => $item,
                 };
-                $stringValue = Str::squish($stringValue);
+                $rowValues = collect($displayHeaders)
+                    ->map(static function ($headerText, $key) use ($iteration, $row, $columnWidths) {
+                        if ('iteration' === $key) {
+                            $rawValue = "{$iteration}";
+                        } else {
+                            $rawValue = $row->{$key} ?? null;
+                        }
+                        $stringValue = match (\gettype($rawValue)) {
+                            'boolean' => $rawValue ? '1' : '0',
+                            'NULL'    => '',
+                            'string'  => (string) str_replace('|', '\|', $rawValue),
+                            default   => (string) $rawValue,
+                        };
+                        $stringValue = Str::squish($stringValue);
 
-                $columnWidths[$key] = max(Str::of($headerText)->length(), Str::of($stringValue)->length(), $columnWidths[$key] ?? 0);
-            }
-        }
+                        return Str::of($stringValue)->padRight($columnWidths[$key]);
+                    })
+                    ->implode(' | ')
+                ;
 
-        $headerCells = [];
-        foreach ($displayHeaders as $key => $headerText) {
-            $headerCells[$key] = Str::of($headerText)->padRight($columnWidths[$key]);
-        }
-        $markdown .= '| ' . implode(' | ', $headerCells) . " |\n";
-
-        $delimiterCells = [];
-        foreach ($displayHeaders as $key => $headerText) {
-            $delimiterCells[$key] = Str::of('')->padRight($columnWidths[$key], '-');
-        }
-        $markdown .= '| ' . implode(' | ', $delimiterCells) . " |\n";
-
-        foreach ($collection as $item) {
-            $row = match (\gettype($item)) {
-                'array' => (object) $item,
-                default => $item,
-            };
-            $rowValues = [];
-            foreach ($displayHeaders as $key => $headerText) {
-                // foreach (array_keys($displayHeaders) as $key) {
-                $rawValue    = $row->{$key} ?? null;
-                $stringValue = match (\gettype($rawValue)) {
-                    'boolean' => $rawValue ? '1' : '0',
-                    'NULL'    => '',
-                    'string'  => (string) str_replace('|', '\|', $rawValue),
-                    default   => (string) $rawValue,
-                };
-                $stringValue = Str::squish($stringValue);
-
-                $rowValues[$key] = Str::of($stringValue)->padRight($columnWidths[$key]);
-            }
-            $markdown .= '| ' . implode(' | ', $rowValues) . " |\n";
-        }
+                return "| {$rowValues} |";
+            })
+            ->implode("\n")
+        ;
+        $markdown .= "{$markdownContent}";
 
         return $markdown;
     }
