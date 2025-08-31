a2877a0f80c9002b9dc34364703356d860f77f32
Tran Ngoc Duc <caothu91@gmail.com>
2025-08-31 07:00:51 +0700

    feat: update lastupdate timestamps and refactor submit method in Baoco and Baono components

--- diepxuan/laravel-catalog/src/Http/Livewire/Cash/Nganhang/Baono.php
+++ diepxuan/laravel-catalog/src/Http/Livewire/Cash/Nganhang/Baono.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
  * @author     Tran Ngoc Duc <ductn@diepxuan.com>
  * @author     Tran Ngoc Duc <caothu91@gmail.com>
  *
- * @lastupdate 2025-06-02 00:36:54
+ * @lastupdate 2025-08-31 06:58:29
  */
 
 namespace Diepxuan\Catalog\Http\Livewire\Cash\Nganhang;
@@ -27,8 +27,10 @@ class Baono extends Component
 
     public function mount(): void
     {
-        \Debugbar::info(\CatalogService::timerFrom());
-        \Debugbar::info(\CatalogService::timerTo());
+        // \Debugbar::info(\CatalogService::timerFrom());
+        // \Debugbar::info(\CatalogService::timerTo());
+        $this->pMa_Nt = \CatalogService::ma_Nt();
+        $this->resultRender();
     }
 
     public function updated($property): void
@@ -38,15 +40,7 @@ class Baono extends Component
 
     public function submit(): void
     {
-        $this->glCts = GlCt::getNKThuChi([
-            'ma_cty'    => \CatalogService::company()->id,
-            'ngay_ct1'  => \CatalogService::timerFrom(),
-            'ngay_ct2'  => \CatalogService::timerTo(),
-            'tk_list'   => $this->pTk_List,
-            'tkdu_list' => $this->pTkdu_List,
-            'ma_bp'     => $this->pMa_Bp ?? '',
-            'ma_nt'     => 'VND',
-        ])->get();
+        $this->resultRender();
     }
 
     /**
@@ -60,5 +54,18 @@ class Baono extends Component
         return view('catalog::cash.nganhang.baono', [
             'glCts' => $this->glCts,
         ])->layout('catalog::layouts.app');
+    }
+
+    protected function resultRender(): void
+    {
+        $this->glCts = GlCt::getNKThuChi([
+            'ma_cty'    => \CatalogService::company()->id,
+            'ngay_ct1'  => \CatalogService::timerFrom(),
+            'ngay_ct2'  => \CatalogService::timerTo(),
+            'tk_list'   => $this->pTk_List,
+            'tkdu_list' => $this->pTkdu_List,
+            'ma_bp'     => $this->pMa_Bp ?? '',
+            'ma_nt'     => 'VND',
+        ])->get();
     }
 }
