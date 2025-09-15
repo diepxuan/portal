c6c7a9cceb95fba767e5cb0ae2f21c6a19703d26
Tran Ngoc Duc <caothu91@gmail.com>
2025-09-15 23:27:26 +0700

    feat: refactor Baono component and update lastupdate timestamp

--- diepxuan/laravel-catalog/src/Http/Livewire/Cash/Nganhang/Baono.php
+++ diepxuan/laravel-catalog/src/Http/Livewire/Cash/Nganhang/Baono.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
  * @author     Tran Ngoc Duc <ductn@diepxuan.com>
  * @author     Tran Ngoc Duc <caothu91@gmail.com>
  *
- * @lastupdate 2025-08-31 06:58:29
+ * @lastupdate 2025-09-14 20:07:23
  */
 
 namespace Diepxuan\Catalog\Http\Livewire\Cash\Nganhang;
@@ -30,7 +30,7 @@ class Baono extends Component
         // \Debugbar::info(\CatalogService::timerFrom());
         // \Debugbar::info(\CatalogService::timerTo());
         $this->pMa_Nt = \CatalogService::ma_Nt();
-        $this->resultRender();
+        // $this->resultRender();
     }
 
     public function updated($property): void
@@ -58,14 +58,24 @@ class Baono extends Component
 
     protected function resultRender(): void
     {
-        $this->glCts = GlCt::getNKThuChi([
-            'ma_cty'    => \CatalogService::company()->id,
-            'ngay_ct1'  => \CatalogService::timerFrom(),
-            'ngay_ct2'  => \CatalogService::timerTo(),
-            'tk_list'   => $this->pTk_List,
-            'tkdu_list' => $this->pTkdu_List,
-            'ma_bp'     => $this->pMa_Bp ?? '',
-            'ma_nt'     => 'VND',
-        ])->get();
+        // $this->glCts = GlCt::getNKThuChi([
+        //     'ma_cty'    => \CatalogService::company()->id,
+        //     'ngay_ct1'  => \CatalogService::timerFrom(),
+        //     'ngay_ct2'  => \CatalogService::timerTo(),
+        //     'tk_list'   => $this->pTk_List,
+        //     'tkdu_list' => $this->pTkdu_List,
+        //     'ma_bp'     => $this->pMa_Bp ?? '',
+        //     'ma_nt'     => 'VND',
+        // ])->get();
+
+        $this->glCts = GlCt::getGLRptNKC03([
+            'pMa_Cty'    => \CatalogService::company()->id,
+            'pNgay_Ct1'  => \CatalogService::timerFrom(),
+            'pNgay_Ct2'  => \CatalogService::timerTo(),
+            'pTk_List'   => $this->pTk_List,
+            'pTkdu_List' => $this->pTkdu_List,
+            'pMa_Nt'     => $this->pMa_Nt,
+            'pMa_Bp'     => $this->pMa_Bp,
+        ]);
     }
 }
