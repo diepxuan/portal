f4cba3176e3eda8e915a620fe2dfdacfea29d2b3
Tran Ngoc Duc <caothu91@gmail.com>
2025-09-15 23:27:26 +0700

    feat: refactor Baono component and update lastupdate timestamp

--- diepxuan/laravel-catalog/resources/views/components/nav-tabs.blade.php
+++ diepxuan/laravel-catalog/resources/views/components/nav-tabs.blade.php
@@ -1,13 +1,28 @@
-@props(['defaultTab'])
+@props(['defaultTab', 'tabs' => []])
 
 <div x-data="{ activeTab: '{{ $defaultTab }}' }" x-on:switch-tab.window="activeTab = $event.detail[0]">
     <div class="mb-4 border-b border-gray-200 print:hidden">
         <ul class="-mb-px flex flex-wrap">
-            {{ $nav }}
+            @empty($tabs)
+                {{ $nav }}
+            @endempty
+            {{-- @dd($tabs) --}}
+            @foreach ($tabs as $tab)
+                <li class="mr-2">
+                    <a href="#" x-on:click.prevent="activeTab = '{{ $tab['key'] }}'"
+                        :class="{
+                            'border-blue-500 text-blue-600': activeTab === '{{ $tab['key'] }}',
+                            'border-transparent hover:text-gray-600 hover:border-gray-300': activeTab !== '{{ $tab['key'] }}'
+                        }"
+                        class="inline-block rounded-t-lg border-b-2 p-4">
+                        {{ $tab['label'] }}
+                    </a>
+                </li>
+            @endforeach
             <li class="mr-2">
                 <a href="#" class="inline-block cursor-default rounded-t-lg p-4">
-                    <svg wire:loading class="text-blue -ml-1 mr-3 h-5 w-5 animate-spin" xmlns="http://www.w3.org/2000/svg"
-                        fill="none" viewBox="0 0 24 24">
+                    <svg wire:loading class="text-blue -ml-1 mr-3 h-5 w-5 animate-spin"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                         <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
                             stroke-width="4"></circle>
                         <path class="opacity-75" fill="currentColor"
@@ -20,6 +35,15 @@
     </div>
 
     <div>
-        {{ $content }}
+        @empty($tabs)
+            {{ $content }}
+        @endempty
+        @foreach ($tabs as $tab)
+            @isset(${$tab['key']})
+                <div x-show="activeTab === '{{ $tab['key'] }}'" class="rounded-lg">
+                    {{ ${$tab['key']} ?? '' }}
+                </div>
+            @endisset
+        @endforeach
     </div>
 </div>
