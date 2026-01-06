2bd6d587e894ad2b72ed287c7e26c542c02ee0b2
Tran Ngoc Duc <caothu91@gmail.com>
2026-01-06 23:18:58 +0700

    feat: replace loading SVGs with reusable component in dashboard and nav-tabs

--- diepxuan/laravel-catalog/resources/views/system/dashboard.blade.php
+++ diepxuan/laravel-catalog/resources/views/system/dashboard.blade.php
@@ -12,43 +12,7 @@
             </td>
             <td>
                 <p class="m-auto cursor-default rounded-t-lg px-3 py-0">
-                    <svg wire:loading class="-ml-1 mr-3 h-5 w-5 animate-spin text-blue-500"
-                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
-                            stroke-width="4"></circle>
-                        <path class="opacity-75" fill="currentColor"
-                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
-                        </path>
-                    </svg>
-                    <!-- Checkmark - hiển thị khi KHÔNG loading -->
-                    @php
-                        $checkmarkId = Str::uuid();
-                    @endphp
-
-                    <svg wire:loading.remove
-                        class="animate-fade-out-{{ $checkmarkId }} -ml-1 mr-3 h-5 w-5 text-green-500"
-                        xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                        <path fill-rule="evenodd"
-                            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                            clip-rule="evenodd" />
-                    </svg>
-                    <style>
-                        .animate-fade-out-{{ $checkmarkId }} {
-                            animation: fadeOut 0.5s ease-out 0.5s forwards;
-                        }
-
-                        @keyframes fadeOut {
-                            from {
-                                opacity: 1;
-                                visibility: visible;
-                            }
-
-                            to {
-                                opacity: 0;
-                                visibility: hidden;
-                            }
-                        }
-                    </style>
+                    <x-icons.loadding />
                 </p>
             </td>
         </tr>
