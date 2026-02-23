4cde493b6bea2667c9c719d867d9082cd00ce60f
Tran Ngoc Duc <ductn@diepxuan.com>
2026-02-23 12:59:49 +0700

    fix: Rename config.php to support.php and fix namespace
    - Rename config.php to support.php to match expected config key 'support'
    - Change namespace from Support::class to string 'Diepxuan\Support'
    - This allows Package::getCommands('support') to work correctly
    - Fixes auto-command registration for support package

--- diepxuan/laravel-support/config/support.php
+++ diepxuan/laravel-support/config/support.php
@@ -11,18 +11,7 @@ declare(strict_types=1);
  * @lastupdate 2025-05-21 11:56:25
  */
 
-use Diepxuan\Support;
-
-/*
- * @copyright  © 2019 Dxvn, Inc.
- *
- * @author     Tran Ngoc Duc <ductn@diepxuan.com>
- * @author     Tran Ngoc Duc <caothu91@gmail.com>
- *
- * @lastupdate 2024-12-12 22:14:08
- */
-
 return [
     'name'      => 'diepxuan/laravel-support',
-    'namespace' => Support::class,
+    'namespace' => 'Diepxuan\Support',
 ];
