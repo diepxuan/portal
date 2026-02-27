1dbb16d005ee11b5854a9f1c2814f11e5f82a2a0
Tran Ngoc Duc <ductn@diepxuan.com>
2026-02-27 21:51:40 +0700

    refactor: shorten ProcedureCaller

--- diepxuan/laravel-simba/src/StoredProcedures/StoredProcedure.php
+++ diepxuan/laravel-simba/src/StoredProcedures/StoredProcedure.php
@@ -8,13 +8,12 @@ declare(strict_types=1);
  * @author     Tran Ngoc Duc <ductn@diepxuan.com>
  * @author     Tran Ngoc Duc <caothu91@gmail.com>
  *
- * @lastupdate 2026-02-26 11:12:40
+ * @lastupdate 2026-02-27 21:46:55
  */
 
 namespace Diepxuan\Simba\StoredProcedures;
 
 use Diepxuan\Simba\SModel\SModel;
-use Illuminate\Support\Collection;
 
 class StoredProcedure
 {
@@ -27,7 +26,7 @@ class StoredProcedure
      *
      * @return mixed kết quả trả về từ procedure (có thể chứa output parameter)
      */
-    public static function call(array $params): Collection
+    public static function call(array $params)
     {
         $connection = (new SModel())->getConnectionName();
 
