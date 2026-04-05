fa8a36b7dabfde2c69f21f1bf3ba95c0f9af1eae
Tran Ngoc Duc <ductn@diepxuan.com>
2026-04-05 23:04:19 +0700

    feat: Add CaPh2 and CaCt2 models for CA4 (Báo nợ)
    - Create CaPh2.php extending CaPh2Model (SModel)
    - Create CaCt2.php extending CaCt2Model (SModel)
    - Required for CA4 (Báo nợ) which uses shared tables with CA2
    - Models follow same pattern as CaPh3/CaCt3 for CA3 (Báo có)

---
+++ diepxuan/laravel-catalog/src/Models/CaCt2.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * @copyright  © 2019 Dxvn, Inc.
+ *
+ * @author     Tran Ngoc Duc <ductn@diepxuan.com>
+ * @author     Tran Ngoc Duc <caothu91@gmail.com>
+ *
+ * @lastupdate 2026-04-05 23:04:19
+ */
+
+namespace Diepxuan\Catalog\Models;
+
+use Diepxuan\Simba\SModel\CaCt2Model as Model;
+
+class CaCt2 extends Model {}
