b2cee38d8f98a8a5d1b13f45ec5dab2ac16bad60
Tran Ngoc Duc <ductn@diepxuan.com>
2026-04-06 08:39:06 +0700

    docs: bổ sung documentation cho 10 file AsCAGet* (Get/Select)

--- diepxuan/laravel-simba/src/StoredProcedures/AsCAGetPH2.php
+++ diepxuan/laravel-simba/src/StoredProcedures/AsCAGetPH2.php
@@ -8,24 +8,66 @@ declare(strict_types=1);
  * @author     Tran Ngoc Duc <ductn@diepxuan.com>
  * @author     Tran Ngoc Duc <caothu91@gmail.com>
  *
- * @lastupdate 2026-02-12 13:15:00
+ * @lastupdate 2026-04-06 08:39:11
  */
 
 namespace Diepxuan\Simba\StoredProcedures;
 
+use Diepxuan\Simba\Helper\ParamHelper;
 use Diepxuan\Simba\SModel\SModel;
 use Illuminate\Support\Collection;
-use Diepxuan\Simba\Helper\ParamHelper;
 
+/**
+ * Class AsCAGetPH2.
+ *
+ * Stored procedure: asCAGetPH2
+ * Mục đích: Lấy danh sách phiếu hàng 2 (PH2) từ hệ thống Simba.
+ * Procedure trả về danh sách các phiếu hàng loại 2 được sử dụng trong quản lý kho.
+ *
+ * Tham số:
+ * - Không có tham số đầu vào.
+ *
+ * Giá trị trả về:
+ * - Collection chứa danh sách phiếu hàng 2 với các trường thông tin liên quan.
+ *
+ * Ví dụ gọi:
+ * ```php
+ * // Sử dụng method call()
+ * $result = AsCAGetPH2::call([]);
+ *
+ * // Sử dụng method callWithParams()
+ * $result = AsCAGetPH2::callWithParams();
+ * ```
+ *
+ * Lưu ý:
+ * - Procedure thuộc nhóm Get (asCAGet*), không có output parameter.
+ * - Sử dụng method callWithParams() để gọi với các tham số đặt tên (nếu có trong tương lai).
+ */
 class AsCAGetPH2
 {
+    /**
+     * Gọi stored procedure asCAGetPH2.
+     *
+     * @param array $params mảng tham số (hiện tại không sử dụng)
+     *
+     * @return Collection kết quả trả về từ procedure
+     */
     public static function call(array $params): Collection
     {
-        $paramObj = ParamHelper::fromArray($params);
+        $paramObj   = ParamHelper::fromArray($params);
         $connection = (new SModel())->getConnectionName();
 
         return ProcedureCaller::call('asCAGetPH2', [
-
         ], $connection);
+    }
+
+    /**
+     * Call stored procedure asCAGetPH2 with named parameters.
+     *
+     * @return Collection kết quả trả về từ procedure
+     */
+    public static function callWithParams(): Collection
+    {
+        return self::call([]);
     }
 }
